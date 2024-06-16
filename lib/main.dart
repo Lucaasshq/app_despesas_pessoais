3a2c2cb00099b059a11aa93020263c9464b07ddf
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-06-16 18:53:37 -0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'package:app_despesas_pessoais/components/transaction_form.dart';
-import 'package:app_despesas_pessoais/components/transaction_list.dart';
-import 'package:app_despesas_pessoais/models/transaction.dart';
+import './components/transaction_user.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 void main() {
   runApp(const ExpensesAppp());
@@ -24,22 +21,7 @@ class ExpensesAppp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  MyHomePage({super.key});
-
-  final _transactions = [
-    Transaction(
-      id: 't1',
-      title: 'Novo Tênis de corrida',
-      value: 310.76,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Conta de Luz',
-      value: 211.30,
-      date: DateTime.now(),
-    )
-  ];
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -48,16 +30,15 @@ class MyHomePage extends StatelessWidget {
         title: const Text('Despesas Pessoais'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: Column(
+      body: const Column(
         children: <Widget>[
-          const SizedBox(
+          SizedBox(
             width: double.infinity,
             child: Card(
               child: Text('Grafico'),
             ),
           ),
-          TransactionList(transactions: _transactions),
-          TransactionForm()
+          TransactionUser(),
         ],
       ),
     );
